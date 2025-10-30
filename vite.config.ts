b04887de78087c4bb738784102afc8788698e2d1
Anna Ok <nevinyatko@gmail.com>
2025-10-30 15:43:27 +0100

    Update repository name to 'public' and configure for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 export default defineConfig({
-  base: '/perso-01/',
+  base: '/public/',
   plugins: [react()],
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
