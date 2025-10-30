8d2ce80c63781c259a70570b11d5e1c16addf2d1
Anna Ok <nevinyatko@gmail.com>
2025-10-30 14:36:04 +0100

    Configure GitHub Pages deployment with correct base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 export default defineConfig({
+  base: '/perso-01/',
   plugins: [react()],
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
